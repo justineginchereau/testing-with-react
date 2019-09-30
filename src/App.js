6efc7bcd30ef0c47637d5ba629e2a96d6bc9538e
justine <justineginchereau@gmail.com>
2019-09-30 15:51:29 -0400

    initial commit of components without tests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import Header from "./component/header";
+import "./app.scss";
 
-function App() {
-  return <div className="App">test</div>;
+// function App() {
+//   return <div className="App">test</div>;
+// }
+
+// export default App;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
